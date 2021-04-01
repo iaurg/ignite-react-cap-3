f20aa7471f0bccdd658835f8fdb82c66ec3c1607
Italo Aurelio <italoaurelior@gmail.com>
2021-04-01 07:13:35 -0300

    Add home page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -6,9 +7,19 @@ export default function Home() {
       <Head>
           <title>Ig.news</title>
       </Head>
-      <h1>      
-        Hello Next.js
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>  
+          </p> 
+        </section>
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
