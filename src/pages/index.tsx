4801a83cf3c1b1b7f74ffdf658823f47d34718fc
Italo Aurelio <italoaurelior@gmail.com>
2021-04-01 08:02:00 -0300

    Intergrate Stripe Service

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import Head from 'next/head'
+import { GetStaticProps } from 'next'
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+type HomeProps = { 
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }:HomeProps) {
   return (
     <>
       <Head>
@@ -16,12 +25,31 @@ export default function Home() {
           </h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>  
+            <span>for {product.amount} month</span>  
           </p> 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps:GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IbNh5DBGZ16Ry7UMMZYQKu0')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 24 horas
+  }
 }
