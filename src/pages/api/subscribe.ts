e2e0c1fa2770250a3ff1d00701c222224a143b58
Italo Aurelio <italoaurelior@gmail.com>
2021-04-02 18:47:18 -0300

    Activate subscriptions with Stripe

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { stripe } from './../../services/stripe';
+import { getSession } from 'next-auth/client';
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  if(request.method === "POST") {
+    const session = await getSession({ req: request })
+
+    console.log(session)
+
+    const stripeCreateCustomer = await stripe.customers.create({
+      email: session.user.email
+    })
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer:stripeCreateCustomer.id,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        { price: 'price_1IbNh5DBGZ16Ry7UMMZYQKu0', quantity: 1}
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL
+    })
+    
+    return response.status(200).json({ sessionId: stripeCheckoutSession.id })
+  } else { 
+    response.setHeader('Allow', "POST")
+    response.status(405).end("Method not allowed")
+  }
+}
