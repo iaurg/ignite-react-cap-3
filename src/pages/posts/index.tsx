9153bf8a263daca41c4472dfbca1026755f4cb4e
Italo Aurelio <italoaurelior@gmail.com>
2021-04-22 06:24:50 -0300

    Integrate Prismic CMS

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import Prismic from '@prismicio/client'
+import { getPrismicClient } from '../../services/prismic'
 import styles from './styles.module.scss'
 
 export default function Posts() {
@@ -30,4 +33,23 @@ export default function Posts() {
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication')
+  ],
+  {
+    fetch: ['publication.title', 'publication.description'],
+    pageSize: 100
+  }
+  )
+
+  console.log(response)
+  
+  return {
+    props: {}
+  }
 }
